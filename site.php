1393ce5ead35a9c17fedcde9e9e6c52988d004b3
Ewelina <ekowalczyk1994@gmail.com>
2022-07-28 21:12:15 -0500

    math operations

--- site.php
+++ site.php
@@ -12,6 +12,8 @@
     <body>
      
     <?php
+    //strings
+
     echo "<h1>My site</h1>";
     $name = "John";
     $age = 35;
@@ -20,6 +22,13 @@
     echo substr($name, 1,2); //create substring from existing string, start from index 1 and cut 2 elements => will return "oh"
     echo "<p> Hello my name is $name and I am $age old. </p>";
 
+    //numbers
+     abs(-100); //return absolute value => will return 100
+     pow(2,4); // will raise 2 to power of 4 => will return 16
+     max(2, 10); //return bigger number
+     round(3.2); //regular round => will return 3
+     ceil(3.3);//round up => will return 4
+     floor(3.8) //round down => will return 3
     ?>
         
         <script src="" async defer></script>
